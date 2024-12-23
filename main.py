ad58c21279b9ef0c2cef4172458eb581648e1ec3
Star156122 <C112156122@nkust.edu.tw>
2024-12-24 03:38:30 +0800

    readme.md

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 
 # 設定你的 Channel Secret 和 Access Token
 LINE_CHANNEL_SECRET = '32c2c2003471e525f684fadf8f2e7966'
@@ -11,13 +11,8 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-# 定義菜單
-MENU = {
-    "1": "宮保雞丁",
-    "2": "紅燒牛肉麵",
-    "3": "清炒時蔬",
-    "4": "麻婆豆腐"
-}
+# 替換成您的圖片直接 URL
+MENU_IMAGE_URL = "https://i.imgur.com/Q1Tm4l6.png"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -41,17 +36,18 @@ def handle_message(event):
     user_message = event.message.text.strip()
 
     if user_message == "菜單":
-        menu_text = "\n".join([f"{key}: {value}" for key, value in MENU.items()])
-        reply = f"今天的菜單:\n{menu_text}\n請輸入編號選擇！"
-    elif user_message in MENU:
-        reply = f"你選擇的是 {MENU[user_message]}，祝你用餐愉快！"
+        # 回傳菜單圖片
+        image_message = ImageSendMessage(
+            original_content_url=MENU_IMAGE_URL,
+            preview_image_url=MENU_IMAGE_URL
+        )
+        line_bot_api.reply_message(event.reply_token, image_message)
     else:
-        reply = "請輸入 '菜單' 查看選項，或輸入正確的編號選擇菜色。"
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply)
-    )
+        reply = "請輸入 '菜單' 查看今天的菜單！"
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply)
+        )
 
 if __name__ == "__main__":
     app.run(port=5000)
