08dd246e854ab1c0832d869b42ea3c70d1445ac7
YUTANG12 <C112156125@nkust.edu.tw>
2024-12-24 11:26:00 +0800

    測試上傳

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 # 設定你的 Channel Secret 和 Access Token
-LINE_CHANNEL_SECRET = '32c2c2003471e525f684fadf8f2e7966'
-LINE_CHANNEL_ACCESS_TOKEN = '7gY9E2XoQ+4NnI2m7FTKRAH5O7imSfGbwoOrxqP2v9kRWDq6ULI+sVdNA0r3yWdeF9n5R17Y/51Zuhm8oGRW38kEN6pmBu+pgHZrvI4yaEdmQmtkCTW3WGgjX+u4iHd71MtCPfsuHCY/yAOtt37PoQdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_SECRET = 'd86079957d0f6e52a7b062828fa323cf'
+LINE_CHANNEL_ACCESS_TOKEN = 'QpmfjEvMXdnw/kZYGXJU7FwbWidXvlRU6+a0zZVjKv8WVQP6XcpA0J7t22a60yWibLH4FEeHkegUC4uBgGh6Rl2ty73UuEshd2v7Vrt+feN3RNyp6pYxKPHgDv5IRjcL+++teLbqBAjfreeICke6JgdB04t89/1O/w1cDnyilFU='
 
 app = Flask(__name__)
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
@@ -16,7 +16,8 @@ MENU = {
     "1": "宮保雞丁",
     "2": "紅燒牛肉麵",
     "3": "清炒時蔬",
-    "4": "麻婆豆腐"
+    "4": "麻婆豆腐",
+    "5": "蒜泥白肉"
 }
 
 @app.route("/callback", methods=['POST'])
@@ -44,7 +45,7 @@ def handle_message(event):
         menu_text = "\n".join([f"{key}: {value}" for key, value in MENU.items()])
         reply = f"今天的菜單:\n{menu_text}\n請輸入編號選擇！"
     elif user_message in MENU:
-        reply = f"你選擇的是 {MENU[user_message]}，祝你用餐愉快！"
+        reply = f"你選擇的是{MENU[user_message]}，祝你用餐愉快！"
     else:
         reply = "請輸入 '菜單' 查看選項，或輸入正確的編號選擇菜色。"
 
