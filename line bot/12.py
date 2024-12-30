cb6c14ca1c0c8ad08d53bc0356b4ef755f7e3f0a
c112156120 <c112156120>
2024-12-30 15:27:49 +0800

    first commit

---
+++ line bot/12.py
@@ -0,0 +1,40 @@
+from linebot import LineBotApi
+from linebot.models import RichMenu, RichMenuArea, RichMenuBounds, URIAction
+
+# 替換為您的 Channel Access Token
+LINE_CHANNEL_ACCESS_TOKEN = 'n5pXYs3fgqex5NYaMCGeO8Ce5UNMQYNYawfyJs1NeqBIuZR9w+gRUPuYZ8G9M2l+VaF8FTar6T8LN0dsNd3a2D7NcGMpVBuE9+4UYILHDw8LboqabOFDaK+KCqpp2PLhLRDE4Flkm6JhjAlwfciXEQdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+
+# 建立圖文選單
+rich_menu_to_create = RichMenu(
+    size={"width": 2500, "height": 843},  # 圖文選單的大小
+    selected=True,
+    name="功能表",  # 圖文選單的名稱
+    chat_bar_text="功能表",  # 用戶點擊時顯示的文字
+    areas=[
+        RichMenuArea(
+            bounds=RichMenuBounds(x=0, y=0, width=833, height=843),
+            action=URIAction(label='註冊', uri='https://example.com/register')
+        ),
+        RichMenuArea(
+            bounds=RichMenuBounds(x=833, y=0, width=833, height=843),
+            action=URIAction(label='菜單', uri='https://example.com/menu')
+        ),
+        RichMenuArea(
+            bounds=RichMenuBounds(x=1666, y=0, width=833, height=843),
+            action=URIAction(label='查詢今日訂購', uri='https://example.com/orders')
+        )
+    ]
+)
+
+# 創建圖文選單
+rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+
+image_path = './rich_menu_image.jpg'
+# 上傳圖文選單圖片
+with open("C:\\Users\\10606\\tmp\\tmp\\rr\\123\\line bot\\rich_menu_image.png", "rb") as image_file:
+
+    line_bot_api.set_rich_menu_image(rich_menu_id, "image/jpeg", image_file)
+
+# 將圖文選單與用戶綁定
+line_bot_api.set_default_rich_menu(rich_menu_id)
